8b28586811ab175a7960d32724a09ea61dd6bf58
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-07-26 18:03:14 +0700

    Create CodeVs.lua

---
+++ CodeVs.lua
@@ -0,0 +1,9 @@
+local codes = {
+    "Release",
+    "BugFix1"
+}
+
+for _, code in ipairs(codes) do
+    task.wait(0.2)
+    game:GetService("ReplicatedStorage").BetweenSides.Remotes.Events.CodesEvent:FireServer("Redeem", "Release")
+end
