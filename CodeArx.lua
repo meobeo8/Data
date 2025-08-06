16c30fd1a29bdd35bcfed00c1dbe173ff5f541fb
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-08-06 12:29:08 +0700

    Update CodeArx.lua

--- CodeArx.lua
+++ CodeArx.lua
@@ -1,17 +1,60 @@
 local codes = {
-    "LiveLike",
-    "10KFollowers",
-    "CongratsFor1M",
-    "SorryForIssues",
-    "Im Back",
-    "125M Visits",
-    "100K CCU",
-    "SORRY4BUGS",
-    "GHOUL!",
-    "UPDATE 1"
+    "LBreset!",
+    "SECRETCODE!",
+    "RiftMode!",
+    "SAOUpd!",
+    "Dungeons!",
+    "MinorChanges!",
+    "EzSoulFrags",
+    "CraftingFix!",
+    "SmartRejoin",
+    "ChainsawUpd!",
+    "GraveyardRaid!",
+    "StatBoosters!",
+    "S3Battlepass!",
+    "SuperSuperSorry!",
+    "3xALLMODES!!",
+    "YOUTUBEBACK!!",
+    "TYBW2!",
+    "QOL2!",
+    "ARXBLEACH!",
+    "Srry4Shutdown",
+    "SmallFixs",
+    "!BrandonTheBest",
+    "!FixBossRushShop",
+    "!TYBW",
+    "!MattLovesARX2",
+    "!RaitoLovesARX",
+    "QuickFix!!",
+    "MoreFixs",
+    "Sorry4AutoTraitRoll",
+    "Sorry4EvoUnits",
+    "SorryDelay!!!",
+    "SummerEvent!",
+    "2xWeekEnd!",
+    "Sorry4Quest",
+    "SorryRaids",
+    "RAIDS",
+    "BizzareUpdate2!",
+    "Sorry4Delays",
+    "HBDTanny",
+    "JoJo Part 1",
+    "NewLobby",
+    "Instant Trait",
+    "CODEISREAL",
+    "ragebait",
+    "PortalsFix",
+    "UPDATE 1.5",
+    "THANKYOU4PATIENCE"
 }
 
+local function redeemCode(code)
+    local success, err = pcall(function()
+        game:GetService("ReplicatedStorage").Remote.Server.Lobby.Code:FireServer(code)
+    end)
+end
+
 for _, code in ipairs(codes) do
+    redeemCode(code)
     task.wait(0.2)
-    game:GetService("ReplicatedStorage").Remote.Server.Lobby.Code:FireServer(code)
 end
