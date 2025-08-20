70a65b2427548c1f041a028e0ce9cbb0355795b3
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-08-21 01:22:42 +0700

    Update CodeHz.lua

--- CodeHz.lua
+++ CodeHz.lua
@@ -1,4 +1,5 @@
 local codes = {"Release","Release2","10KEVENT","Sorryhaha","10KLIKES","ISCODEWORKING"}
-for _,code in ipairs(codes) do
-    game:GetService("ReplicatedStorage").Packets.RedeemCode:InvokeServer(code) task.wait(1)
+for _, code in ipairs(codes) do
+    game:GetService("ReplicatedStorage").Packets.RedeemCode:InvokeServer(code)
+    task.wait(1)
 end
