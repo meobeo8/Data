5927ecf27f9c35e1b6d80b4a39571bc93613b50d
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-08-24 07:55:33 +0700

    Update CodeHz.lua

--- CodeHz.lua
+++ CodeHz.lua
@@ -1,29 +1,11 @@
 local codes = {
-    "60KLIKES",     -- 30k Coins
-    "20KIN",        -- 20k Coins
-    "70KLIKES",     -- 5 Lucky Weapon Spins
-    "sorryfordelay",-- 15 Spins
-    "10KFOL1",      -- 20 Lucky Weapon Spins
-    "10KFOL2",      -- 20 Lucky Perk Spins
-    "10KFOL3",      -- 22,222 Coins
-    "10KFOL4",      -- 5 Traits
-    "50KLIKES",     -- 55,555 Coins
-    "10KLIKES",     -- 10 Lucky Weapon Spins
-    "20KLIKES",     -- 5 Traits
-    "30KLIKES",     -- 10 Lucky Perk Spins
-    "100KMEMBERS",  -- 100k Coins
-    "40KLIKES",     -- 5 Traits
-    "Release",      -- 5 Lucky Weapon Spins
-    "Release2",     -- 20000 Cash
-    "10KEVENT",     -- 30000 Cash
-    "Sorryhaha",    -- 5 Lucky Perk Spins
-    "150KLIKES",    -- 15k Coins
-    "120KIN",       -- 30k Coins
-    "Scythecool",   -- 10 Spins
-    "Hzdelay",      -- 50k Coins
-    "Hugecode",     -- 150k Coins
-    "WDEV1",        -- 5 Lucky Weapon Spins
-    "WDEV2"         -- 5 Traits
+    "150KLIKES", -- 15k Coins
+    "120KIN",    -- 30k Coins
+    "Scythecool",-- 10 Spins
+    "Hzdelay",   -- 50k Coins
+    "Hugecode",  -- 150k Coins
+    "WDEV1",     -- 5 Lucky Weapon Skins
+    "WDEV2"      -- 5 Traits
 }
 
 for _, code in ipairs(codes) do
