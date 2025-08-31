64db3828ebc3705f75ce55dca2c68f66aae8b0b1
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-08-31 16:03:08 +0700

    Update CodeHz.lua

--- CodeHz.lua
+++ CodeHz.lua
@@ -1,15 +1,11 @@
 local codes = {
-    "sorryforbug",
-    "200KCCU",
-    "NEWPLAYER1",
-    "200KIN",
     "DAGGER",
-    "300KLIKES"
+    "200KIN",
+    "NEWPLAYER1",
+    "200KCCU"
 }
 
 for _, code in ipairs(codes) do
-    pcall(function()
-        game:GetService("ReplicatedStorage").Packets.RedeemCode:InvokeServer(code)
-    end)
-    task.wait(0.5)
+    game:GetService("ReplicatedStorage").Packets.RedeemCode:InvokeServer(code)
+    task.wait(1)
 end
