6b7e1b02f6c29b8baf6e39067ca7dfb568d3cdb4
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2025-09-11 21:14:23 +0700

    Delete CodeUp.lua

--- CodeUp.lua
+++
@@ -1,9 +0,0 @@
-local codes = {
-    "UPD0.75",
-    "UPDATE1"
-}
-
-for _, code in ipairs(codes) do
-    task.wait(0.2)
-    game:GetService("ReplicatedStorage").Event.MainCode:InvokeServer(code)
-end
